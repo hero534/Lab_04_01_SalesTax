4b097c357f2f86d8e321ebb66e087fdc6d0cbd41
hgold <goldsmhr@mail.uc.edu>
2025-02-03 20:05:07 -0500

    Task 01 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        double itemPrice = 0.00;
+        double salesTax = 0.05;
+        double totalPrice = 0.00;
+            System.out.println("What is the price of your item?");
+        itemPrice = in.nextDouble();
+        totalPrice = itemPrice * salesTax;
+            System.out.println("The sales tax of " + itemPrice + " is " + totalPrice);
     }
 }
